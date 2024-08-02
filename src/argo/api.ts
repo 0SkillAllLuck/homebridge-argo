472b5848fa0a695814999257c6fd931efd7c881a
Cedric Lewe <0SkillAllLuck@pm.me>
2024-08-02 14:22:46 +0200

    Add debounce for write operations

--- src/argo/api.ts
+++ src/argo/api.ts
@@ -6,6 +6,7 @@ export class ArgoApi {
   private readonly request: AxiosInstance;
 
   private nextHMI = DEFAULT_HMI;
+  private debounce = Date.now();
 
   constructor(
     ip: string,
@@ -21,7 +22,7 @@ export class ArgoApi {
 
   // Check if there is a pending HMI update
   pending(): boolean {
-    return this.nextHMI !== DEFAULT_HMI;
+    return this.nextHMI !== DEFAULT_HMI && Date.now() >= this.debounce;
   }
 
   // Sync the HMI from the device, if required also update the HMI
@@ -39,41 +40,44 @@ export class ArgoApi {
   }
 
   // Set the target temperature
-  setTargetTemperature(temperature: number): void{
+  setTargetTemperature(temperature: number): void {
     this.setHMI(temperature.toString(), 0);
   }
 
   // Set the operating mode to either OFF (0) or ON (1)
-  setOperatingMode(mode: 0 | 1): void{
+  setOperatingMode(mode: 0 | 1): void {
     this.setHMI(mode.toString(), 2);
   }
 
   // Set the operation mode to either COOL (1), DRY (2), HEAT (3), FAN (4), or AUTO (5)
-  setOperationMode(mode: 1 | 2 | 3 | 4 | 5): void{
+  setOperationMode(mode: 1 | 2 | 3 | 4 | 5): void {
     this.setHMI(mode.toString(), 3);
   }
 
   // Set the fan mode to either AUTO (0), LOWEST (1), LOW (2), MEDIUM (3), HIGH (4), HIGHER (5), or HIGHEST (6)
-  setFanMode(mode: 0 | 1 | 2 | 3 | 4 | 5 | 6): void{
+  setFanMode(mode: 0 | 1 | 2 | 3 | 4 | 5 | 6): void {
     this.setHMI(mode.toString(), 4);
   }
 
   // Turns off (0) or on (1) the eco mode
-  setEcoMode(mode: 0 | 1): void{
+  setEcoMode(mode: 0 | 1): void {
     this.setHMI(mode.toString(), 8);
   }
 
   // Turns off (0) or on (1) the turbo mode
-  setTurboMode(mode: 0 | 1): void{
+  setTurboMode(mode: 0 | 1): void {
     this.setHMI(mode.toString(), 9);
   }
 
   // Turns off (0) or on (1) the night mode
-  setNightMode(mode: 0 | 1): void{
+  setNightMode(mode: 0 | 1): void {
     this.setHMI(mode.toString(), 10);
   }
 
   private setHMI(value: string, index: number) {
+    // Increase the debounce time to help bundling write requests
+    this.debounce = Date.now() + 250;
+
     // Update the HMI with the new value
     const parts = this.nextHMI.split(',');
     parts[index] = value;
